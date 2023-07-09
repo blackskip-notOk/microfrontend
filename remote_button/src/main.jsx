2de6737986e65873a757d2b90a4184645577d7d1
Ilya Babanov <ilbabanov@gmail.com>
2023-07-09 14:48:50 +0400

    remove all

--- remote_button/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client.js';
-import Button from './Button.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<Button />
-	</React.StrictMode>,
-);
