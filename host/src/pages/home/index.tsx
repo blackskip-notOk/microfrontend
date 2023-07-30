f1aeab5847907e806043f7fb2902b9ba2b9370ce
Ilya Babanov <ilbabanov@gmail.com>
2023-07-30 14:34:44 +0400

    add some colors

--- host/src/pages/home/index.tsx
+++ host/src/pages/home/index.tsx
@@ -1,6 +1,21 @@
 import { path } from '~pages/router';
 import type { FC } from 'react';
-import { container, header, title,  navbar, footer } from './index.module.css';
+import {
+	container,
+	header,
+	title,
+	navbar,
+	content,
+	footer,
+	color,
+	brand,
+	text1,
+	text2,
+	surface1,
+	surface2,
+	surface3,
+	surface4,
+} from './index.module.css';
 
 import { Link, Outlet } from 'react-router-dom';
 
@@ -8,7 +23,7 @@ export const HomePage: FC = () => {
 	return (
 		<main className={container}>
 			<header className={header}>
-				<h1 className={title} >Home Sweet Home</h1>
+				<h1 className={title}>Home Sweet Home</h1>
 			</header>
 			<nav className={navbar}>
 				<ul>
@@ -17,7 +32,18 @@ export const HomePage: FC = () => {
 					</li>
 				</ul>
 			</nav>
-			<Outlet />
+			<section className={content}>
+				<div>
+					<div className={[color, brand].join(' ')} />
+					<div className={[color, text1].join(' ')} />
+					<div className={[color, text2].join(' ')} />
+					<div className={[color, surface1].join(' ')} />
+					<div className={[color, surface2].join(' ')} />
+					<div className={[color, surface3].join(' ')} />
+					<div className={[color, surface4].join(' ')} />
+				</div>
+				<Outlet />
+			</section>
 			<footer className={footer}>Footer</footer>
 		</main>
 	);
