5d35b010926ce10f1dc5d0a865e8a6e63e16cdeb
Ilya Babanov <ilbabanov@gmail.com>
2023-08-20 10:38:32 +0400

    f

--- host/src/pages/index.ts
+++ host/src/pages/index.ts
@@ -1,4 +1,4 @@
-export { HomePage } from './home/HomePage';
+export { HomePage } from './home/homePage';
 export { LoginPage } from './login/LoginPage';
 export { SignupPage } from './signup/SignupPage';
 export { UsersPage } from './users/UsersPage';
